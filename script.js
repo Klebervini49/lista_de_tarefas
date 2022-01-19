851ce68c0ca07f2e86b7d74c0704d4ae68650829
Kleberson <klebervini49@gmail.com>
2022-01-18 22:20:57 -0300

    FIX

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 let array = []
 Object.keys(localStorage).forEach(function (key) {
-    document.querySelector('.tarefasList').innerHTML = `${document.querySelector('.tarefasList').innerHTML}<li class='itemTarefa'>${localStorage.getItem(key)}</li>`
+    document.querySelector('.tarefasList').innerHTML += `<div><label class='itemTarefa'>${localStorage.getItem(key)}</label></div>`
+
 });
 
 document.querySelector('button.addRmv--tarefa').addEventListener('click', () => {
     let ultimoItem = document.querySelector('.writeList').value
     if (ultimoItem != '') {
         array.push(ultimoItem)
-        localStorage.setItem(`${localStorage.length + 1}`, ultimoItem)
-        document.querySelector('.tarefasList').innerHTML = `${document.querySelector('.tarefasList').innerHTML} <li class='itemTarefa'>${ultimoItem}</li>`
+        localStorage.setItem(`${localStorage.length + 1}`, ultimoItem, 'eae')
+        document.querySelector('.tarefasList').innerHTML += `<div class='tarefa${localStorage.length}'><label for"${localStorage.length+1}" class='itemTarefa'>${ultimoItem}</label></div>`
     } else {
         alert('Por favor, escreva algo.')
     }
